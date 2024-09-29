d24710811236e6da286df6630e28e149555edc54
Ron Jacob Dinero <ronjacobdinero15@gmail.com>
2024-09-29 21:44:46 +0800

    Update handleOrder.php

--- Numba 3/handleOrder.php
+++ Numba 3/handleOrder.php
@@ -13,25 +13,34 @@
                 $quantity = $_POST['quantity'];
                 $cash = $_POST['cash'];
 
-                $total = $order*$quantity;
+                $total = $order * $quantity;
                 $change = $cash - $total;
 
-                $enoughBalance = $total > $cash;
-            }
-        ?>
+                $enoughBalance = $cash >= $total;
 
-        <h1 style="text-align: center;">RECEIPT</h1>
-        
-        <div>
-            <p>Total price: <?php echo $total;?></p>
-            <p>You paid: <?php echo $cash;?></p>
-            <p>CHANGE: <?php echo $change;?></p>
-            <div id="current_date">
-                <script>
-                    document.getElementById("current_date").innerHTML = Date();
-                </script>
-            </div>
-        </div>
+                if($enoughBalance) { 
+                    // Show receipt if balance is enough
+        ?>
+                    <h1 style="text-align: center;">RECEIPT</h1>
+                    <div>
+                        <p>Total price: <?php echo $total;?></p>
+                        <p>You paid: <?php echo $cash;?></p>
+                        <p>CHANGE: <?php echo $change;?></p>
+                        <div id="current_date">
+                            <script>
+                                document.getElementById("current_date").innerHTML = Date();
+                            </script>
+                        </div>
+                    </div>
+        <?php 
+                } else { 
+                    // Show error message if balance is not enough
+        ?>
+                    <p style="color:red; text-align:center;">You do not have enough balance!</p>
+        <?php 
+                }
+            } 
+        ?>
     </div>
 </body>
 </html>
